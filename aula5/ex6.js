5739d3983ea9eb1981c9f11ec96c4d0ee9b19ef9
goesq <victorgoes145@gmail.com>
2024-08-28 20:17:06 -0300

    atualização matriz

--- aula5/ex6.js
+++ aula5/ex6.js
@@ -3,3 +3,6 @@ nomes.forEach(function(nomes, index)    {
     console.log(index,nomes)
 
 });
+for (i=0;i<nomes.length;i++){
+    console.log(` ${i} ${nomes[i]}` )
+}
--- aula5/ex7.js
+++ aula5/ex7.js
@@ -1,3 +1,4 @@
 const numeros = [1,2,3,4]
 const quadrados = numeros.map(num => num*num)
-console.log()
+// map é uma função que aplica na lista para elevar os numeros ao quadrado
+console.log(quadrados)
