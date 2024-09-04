b0721d0a87fa50a59fdd69fd9c0af17a448300e2
goesq <victorgoes145@gmail.com>
2024-09-04 20:40:35 -0300

    Update

---
+++ aula5/exercicios_casa/ex2.js
@@ -0,0 +1,11 @@
+let elemento1 = [];
+
+elemento1.push(10);
+elemento1.push(20);
+elemento1.push(30);
+
+console.log('Array antes da alteração:', elemento1);
+
+elemento1[0] = elemento1[0] * 2;
+
+console.log('Array após a alteração:', elemento1);
---
+++ aula5/exercicios_casa/ex3.js
@@ -0,0 +1,7 @@
+let elemento1 = [];
+
+elemento1.push(5);
+elemento1.push(10);
+elemento1.push(15);
+
+console.log('Array resultante:', elemento1);
